36f1e4e516fa4388372bca4d05cfff613e980d11
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-07-20 00:05:33 -0300

    feat: adiciona 'AllowAny' ao ProductViewSet e define queryset diretamente

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -5,7 +6,6 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        return Product.objects.all()
