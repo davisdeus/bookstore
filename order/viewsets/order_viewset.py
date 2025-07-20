86f1ee56f19d12d65913a1b3369b6aedb71704b3
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-07-19 23:56:26 -0300

    feat: permite acesso irrestrito ao endpoint de pedidos - Adiciona 'AllowAny' nas permission_classes do OrderViewSet - Reorganiza atributos da classe para melhor legibilidade

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    serializer_class = OrderSerializer
+    permission_classes = [AllowAny]
     queryset = Order.objects.all()
+    serializer_class = OrderSerializer
