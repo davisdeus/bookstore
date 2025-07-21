268a5cef9f8aa93831dd11b96df08ae9c4d0db7e
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-07-20 23:37:03 -0300

    fix(product): remove autenticação da ProductViewSet e permite acesso público
    - Substitui IsAuthenticated por AllowAny
    - Remove classes de autenticação desnecessárias
    - Garante que rota de produtos esteja acessível

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,5 +1,4 @@
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -7,8 +6,7 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]  
     serializer_class = ProductSerializer
 
     def get_queryset(self):
