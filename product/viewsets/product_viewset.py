e0b4c6f886fa2ef7cc5344ea50f02a74515f8912
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-07-20 22:42:54 -0300

    refactor(product): protege ProductViewSet com autenticação e ordena produtos por ID

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -1,4 +1,5 @@
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from product.models import Product
@@ -6,6 +7,9 @@ from product.serializers.product_serializer import ProductSerializer
 
 
 class ProductViewSet(ModelViewSet):
-    permission_classes = [AllowAny]
-    queryset = Product.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by("-id")
