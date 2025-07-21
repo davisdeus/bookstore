343563e3f4d2c5b130739fdf2caa82f775c9b0ff
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2025-07-20 22:31:19 -0300

    test(order): ajusta testes para lidar com estrutura de resposta com 'results'

--- order/test/test_order_viewset.py
+++ order/test/test_order_viewset.py
@@ -24,26 +24,30 @@ class TestOrderViewSet(APITestCase):
         response = self.client.get(reverse("order-list", kwargs={"version": "v1"}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        order_data = json.loads(response.content)[0]
-        self.assertEqual(order_data["product"][0]["title"], self.product.title)
-        self.assertEqual(order_data["product"][0]["price"], self.product.price)
-        self.assertEqual(order_data["product"][0]["active"], self.product.active)
+        order_data = json.loads(response.content)
         self.assertEqual(
-            order_data["product"][0]["category"][0]["title"], self.category.title
+            order_data["results"][0]["product"][0]["title"], self.product.title
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["price"], self.product.price
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["active"], self.product.active
+        )
+        self.assertEqual(
+            order_data["results"][0]["product"][0]["category"][0]["title"],
+            self.category.title,
         )
 
     def test_create_order(self):
         user = UserFactory()
         product = ProductFactory()
-        data = json.dumps({
-            'products_id': [product.id],
-            'user': user.id
-        })
+        data = json.dumps({"products_id": [product.id], "user": user.id})
 
         response = self.client.post(
             reverse("order-list", kwargs={"version": "v1"}),
-            data = data,
-            content_type="application/json"
+            data=data,
+            content_type="application/json",
         )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
