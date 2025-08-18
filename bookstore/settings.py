d129d4a29a6249d41a67496ec98e496505cff8bd
Davi Silva de Deus <101847163+davisdeus@users.noreply.github.com>
2025-08-17 23:11:15 -0300

    Update settings.py
    Evita carregar a Debug Toolbar durante testes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -49,6 +49,13 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+import sys
+
+# Evita carregar a Debug Toolbar durante testes
+if DEBUG and not any(arg in sys.argv for arg in ["test", "pytest"]):
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
 ROOT_URLCONF = "bookstore.urls"
 
 TEMPLATES = [
